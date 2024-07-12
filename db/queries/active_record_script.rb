dfce4dd42f779e340d93178738901d9caa637280
DT_Pratham <pratham.madhunapanthula@dispatchtrack.com>
2024-07-12 14:03:47 +0530

    queries

--- db/queries/active_record_script.rb
+++ db/queries/active_record_script.rb
@@ -5,7 +5,7 @@ players_older_than_age = Player.where('age> ?', 25)
 
 count_all_players = Player.group(:teams_id).count
 
-destroying = Player.destroy(name: 'Player1')
+destroying_player = Player.destroy(name: 'Player1')
 
 new_player = Player.create(name: 'playerNew', age:'24', position:'midfielder',teams_id:34)
 
